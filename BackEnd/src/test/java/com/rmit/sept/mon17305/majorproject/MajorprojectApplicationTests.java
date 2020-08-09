713297a0f9aafde645128a372fabda9eaadbbe84
Chiraporn Ropkhop s3781279 <s3781279@student.rmit.edu.au>
2020-08-09 15:23:16 +1000

    start major project skeleton

---
+++ BackEnd/src/test/java/com/rmit/sept/mon17305/majorproject/MajorprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rmit.sept.mon17305.majorproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MajorprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
