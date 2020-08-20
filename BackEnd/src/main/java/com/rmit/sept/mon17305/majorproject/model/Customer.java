de6e2d2aff8125717debadbc6406c73f23a666c5
Evzy <evzyman@hotmail.com>
2020-08-20 21:10:35 +1000

    rebasing

--- BackEnd/src/main/java/com/rmit/sept/mon17305/majorproject/model/Customer.java
+++ BackEnd/src/main/java/com/rmit/sept/mon17305/majorproject/model/Customer.java
@@ -2,6 +2,79 @@ package com.rmit.sept.mon17305.majorproject.model;
 
 import javax.persistence.Entity;
 
+<<<<<<< HEAD
 @Entity
 public class Customer extends User{
+=======
+public class Customer extends User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    //@NotBlank - add dependency
+    private String firstName;
+    private String lastName;
+    private String type;
+    private Date created_At;
+    private Date updated_At;
+
+    public Customer() {
+        super();
+    }
+
+    @Override
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public String getFirstName() {
+        return firstName;
+    }
+
+    @Override
+    public String getLastName() {
+        return lastName;
+    }
+
+    @Override
+    public String getType() {
+        return type;
+    }
+
+    @Override
+    public Date getCreated_At() {
+        return created_At;
+    }
+
+    @Override
+    public Date getUpdated_At() {
+        return updated_At;
+    }
+
+    @Override
+    public void setId(Long id1) {
+        this.id = id1;
+    }
+
+    @Override
+    public void setFirstName(String fName) {
+        this.firstName = fName;
+    }
+
+    @Override
+    public void setLastName(String lName) {
+        this.lastName = lName;
+    }
+
+    @Override
+    public void setType(String type1) {
+        this.type = type1;
+    }
+
+    @Override
+    public void setUpdated_At(Date date1) {
+        this.updated_At = date1;
+    }
+>>>>>>> f4884a3... Merged with model
 }
--- BackEnd/src/main/java/com/rmit/sept/mon17305/majorproject/model/User.java
+++ BackEnd/src/main/java/com/rmit/sept/mon17305/majorproject/model/User.java
@@ -7,4 +7,49 @@ public abstract class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+<<<<<<< HEAD
+=======
+    //@NotBlank - add dependency
+    private String firstName;
+    private String lastName;
+    private String type;
+    private Date created_At;
+    private Date updated_At;
+
+    public User() {
+    }
+
+    public abstract Long getId();
+
+    public abstract String getFirstName();
+
+    public abstract String getLastName();
+
+    public abstract String getType();
+
+    public abstract Date getCreated_At();
+
+    public abstract Date getUpdated_At();
+
+    public abstract void setId(Long id1);
+
+    public abstract void setFirstName(String fName);
+
+    public abstract void setLastName(String lName);
+
+    public abstract void setType(String type1);
+
+    public abstract void setUpdated_At(Date date1);
+
+
+    @PrePersist
+    protected void onCreate() {
+        this.created_At = new Date();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updated_At = new Date();
+    }
+>>>>>>> f4884a3... Merged with model
 }
