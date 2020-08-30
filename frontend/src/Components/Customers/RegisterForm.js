97512a7ab1208705768cf7cf42bb958ff87fd608
Chiraporn Ropkhop s3781279 <s3781279@student.rmit.edu.au>
2020-08-30 15:30:10 +1000

    linked new customer register to back end - to be reviewed

--- frontend/src/Components/Customers/RegisterForm.js
+++ frontend/src/Components/Customers/RegisterForm.js
@@ -1,43 +1,89 @@
 import React from 'react';
 import './LoginForm.css'
 import NavigationBar from './NavigationBar';
-import {Link as LinkRouter} from "react-router-dom";
+import {Link as LinkRouter, useHistory} from "react-router-dom";
 
-function RegisterForm(){
+function RegisterForm(props){
+    const history = useHistory();
+    const[userDetails, setUserDetails] = useState({
+        username: "",
+        firstname:"",
+        lastname: "",
+        password: "",
+        billingAdd:"",
+        shipppingAdd:""
+    });
+
+    function handleChange(event){
+        const{name, value} = event.target;
+        setUserDetails(prevValue =>{
+          return {
+            ...prevValue,
+            [name] : value
+          }
+        })
+    }
+
+    async function createCustomer(user){
+        try {
+        const res = await axios.post("http://localhost:8080/api/customer", person);
+            props.history.push("/");
+         } catch (err) {
+        console.log(err);
+
+       }
+    }
+
+    function handleSubmit(event) {
+        console.log("handleSubmit");
+        event.preventDefault();
+        const person = {
+          username: userDetails.username,
+          firstName: userDetails.finishtime,
+          lastName:userDetails.lastname,
+          password:userDetails.password,
+          billingAdd: userDetails.billingAdd,
+          shipppingAdd: userDetails.shipppingAdd
+        }
+        createCustomer(person);
+    }
+    
     return(
         <div>
             <NavigationBar/>
             <br></br>
             <br></br>
             <br></br>
-            <form className = "entryRegister" id = "registerForm" onSubmit="">
+            <form className = "entryRegister" id = "registerForm" onSubmit={handleSubmit}>
             <h1 className = "regoHead">Join Us!</h1>
             <label className = "regoLabel">Join us today to have a wide range of services, right at your fingertips!</label>
             <br></br>
             <br></br>
             <label className = "regoLabel">First Name</label><br></br>
-            <input type = "text" id = "firstname" className = "loginInput" placeholder = "Enter First Name" required></input>
+            <input type = "text" id = "firstname" onChange={handleChange} name="firstname" className = "loginInput" placeholder = "Enter First Name" required></input>
             <br></br>
             <br></br>
             <label className = "regoLabel">Last Name</label><br></br>
-            <input type = "text" id = "lastname" className = "loginInput" placeholder = "Enter Last Name" required></input>
+            <input type = "text" id = "lastname" onChange={handleChange} name="lastname" className = "loginInput" placeholder = "Enter Last Name" required></input>
             <br></br>
             <br></br>
             <label className = "regoLabel">Username</label><br></br>
-            <input type = "text" id = "username" className = "loginInput" placeholder = "Enter Username" required></input>
+            <input type = "text" id = "username" onChange={handleChange} name="username" className = "loginInput" placeholder = "Enter Username" required></input>
             <br></br>
             <br></br>
             <label className = "regoLabel">Password</label><br></br>
-            <input type = "password" id = "password" className = "loginInput" placeholder = "Enter Password" required></input>
+            <input type = "password" id = "password" onChange={handleChange} name="password" className = "loginInput" placeholder = "Enter Password" required></input>
             <br></br>
             <br></br>
-            <LinkRouter to = "/signup">
+           {//<LinkRouter to = "/signup">
+           // </LinkRouter>
+    }
             <div className = "buttonholder">
                 <input id = "registerBtn" type="submit" value="Sign Up"></input>
             </div>
             <br>
             </br>
-            </LinkRouter>
+           
             <label className = "regoLabel">Already have an account? <LinkRouter to = "/loginform">Log In </LinkRouter> here!</label>
             <br></br>
             <br></br>
