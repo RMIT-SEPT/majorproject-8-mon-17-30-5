25791bd8f4e8eef5b46f0ea8970f9eb49807d4e7
Chiraporn Ropkhop s3781279 <s3781279@student.rmit.edu.au>
2020-09-02 15:09:19 +1000

    retrieve bookings for a customer

--- frontend/src/Components/Customers/DisplayABooking.js
+++ frontend/src/Components/Customers/DisplayABooking.js
@@ -1,23 +1,46 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 
 function DisplayABooking(props) {
+    
+    const[userDetails, setUserDetails] = useState({
+        description:"",
+        workerName:"",
+        startTime:"null",
+        finishTime:"null"
+    });
 
-    // private Long workerId; 
-    // private String workerName;
-    // @NotNull (message = "Service ID is required")
-    // private Long serviceId; --> getdescription
-    // private String type;
-    // private Date startTime;
-    // private Date finishTime;
+    useEffect(() => {
+       getDescription();
+    }, []);
+
+    function getDescription(){
+        
+        axios.get("http://localhost:8080/api/serviceObject/"+props.booking.serviceId)
+        .then((response)=>{
+            console.log(response.data);
+            setUserDetails  ((prevValue)=>{
+            return {
+                ...prevValue,
+                description: response.data.description,
+                workerName: response.data.workerName
+            }
+          });       
+        })
+        .catch()
+        .finally();
+    }
+
     return (
         <div>
             <p>Id {props.booking.id}</p>
             <ul>
-                <li>description:{}</li>
-                <li>worker Id: {}</li>
-                <li>worker name: {}</li>
-                <li>Start time: {}</li>
-                <li>Finish time: {}</li>
+                <li>Service Id: {props.booking.serviceId}</li>
+                <li>description:{userDetails.description}</li>
+                <li>worker Id: {props.booking.workerId}</li>
+                <li>worker name: {userDetails.workerName}</li>
+                <li>Start time: {userDetails.startTime}</li>
+                <li>Finish time: {userDetails.finishTime}</li>
             </ul>
         </div>
     )
