757b4e5fbf17ce50a2fb0f443a501549c997ef2a
cindy859 <57559043+cindy859@users.noreply.github.com>
2020-09-17 23:06:51 +0700

    add test for error page

--- frontend/src/Components/Admins/gotopage.test.js
+++ frontend/src/Components/Admins/gotopage.test.js
@@ -5,8 +5,18 @@ import { MemoryRouter } from 'react-router';
 import BookingPage from './AdminDashboard';
 import WorkerPage from './AdminDashboard';
 import App from './src/App';
+import ErrorPage from './src/Components/Layout/ErrorPage';
 
-jest.mock('firebase/app');
+  test('invalid path should redirect to error page', () => {
+    const wrapper = mount(
+      <MemoryRouter initialEntries={[ '/' ]}>
+        <App/>
+      </MemoryRouter>
+    );
+    expect(wrapper.find(WorkerPage)).toHaveLength(0);
+    expect(wrapper.find(BookingPage)).toHaveLength(0);
+    expect(wrapper.find(ErrorPage)).toHaveLength(1);
+  });
 
 test('valid path for booking page', () => {
   const wrapper = mount(
@@ -15,6 +25,7 @@ test('valid path for booking page', () => {
     </MemoryRouter>
   );
   expect(wrapper.find(BookingPage)).toHaveLength(1);
+  expect(wrapper.find(ErrorPage)).toHaveLength(0);
 });
 
 test('valid path for worker page', () => {
@@ -24,4 +35,5 @@ test('valid path for worker page', () => {
       </MemoryRouter>
     );
     expect(wrapper.find(WorkerPage)).toHaveLength(1);
+    expect(wrapper.find(ErrorPage)).toHaveLength(0);
   });
