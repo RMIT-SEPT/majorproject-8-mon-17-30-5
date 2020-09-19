15572e39a9a3c1daecdde75327d537c5d180cdaf
cindy859 <57559043+cindy859@users.noreply.github.com>
2020-09-19 23:31:02 +0700

    fix fail error for AdminDashboardLinkPath

--- frontend/src/Components/Admins/AdminDashboardLinkPath.test.js
+++ frontend/src/Components/Admins/AdminDashboardLinkPath.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { mount } from 'enzyme';
+import { mount, configure } from 'enzyme';
 import { MemoryRouter } from 'react-router';
 import BookingPage from './AdminDashboard';
 import WorkerPage from './AdminDashboard';
