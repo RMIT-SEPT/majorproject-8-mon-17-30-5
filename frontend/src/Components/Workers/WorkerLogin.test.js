8928e0bd0dceafcecbffd2daaf459fd3de4705b1
s3718362 <s3718362@student.rmit.edu.au>
2020-09-20 14:35:51 +1000

    All tests pass

--- frontend/src/Components/Workers/WorkerLogin.test.js
+++ frontend/src/Components/Workers/WorkerLogin.test.js
@@ -1,42 +1,16 @@
 
+  
 import React from 'react';
-import { shallow } from 'enzyme';
 import WorkerLogin from './WorkerLogin';
-import Enzyme from "enzyme";
-import ShallowRenderer from 'react-test-renderer/shallow';
 import {cleanup} from '@testing-library/react';
+import ShallowRenderer from 'react-test-renderer/shallow';
 
 afterEach(cleanup);
-configure({ adapter: new Adapter() });
 
-
-// describe('Test cases for testing WorkerLogin', () => {
-//     let wrapper;
-//     test('username check', () => {
-//         wrapper = shallow(<WorkerLogin />);
-//         wrapper.find('input[type="text"]').simulate('change', { target: { name: 'username', value: 'john' } });
-//         expect(wrapper.state('username')).toEqual('john');
-//     })
-
-//     it('password check', () => {
-//         wrapper = shallow(<WorkerLogin />);
-//         wrapper.find('input[type="password"]').simulate('change', { target: { name: 'password', value: '1234' } });
-//         expect(wrapper.state('password')).toEqual('1234');
-//     })
-
-//     it('WorkerLogin check for correct login information', () => {
-//         wrapper = shallow(<WorkerLogin />);
-//         wrapper.find('input[type="text"]').simulate('change', { target: { name: 'username', value: 'john' } });
-//         wrapper.find('input[type="password"]').simulate('change', { target: { name: 'password', value: '1234' } });
-//         wrapper.find('button').simulate('click');
-//         expect(wrapper.state('hasWorkerLoginFailed')).toBe(true);
-//     })
-
-//     it('WorkerLogin check for wrong login information', () => {
-//         wrapper = shallow(<WorkerLogin />);
-//         wrapper.find('input[type="text"]').simulate('change', { target: { name: 'username', value: 'john' } });
-//         wrapper.find('input[type="password"]').simulate('change', { target: { name: 'password', value: '123' } });
-//         wrapper.find('button').simulate('click');
-//         expect(wrapper.state('hasWorkerLoginFailed')).toBe(false);
-//     })
-// })
+it("render admin login correctly", () =>{
+    const renderer = new ShallowRenderer();
+    renderer.render(<WorkerLogin/>);
+    const result = renderer.getRenderOutput();
+    expect(result.type).toBe('div');
+    expect(result.props.children).toHaveLength(5);
+})
--- frontend/src/Components/Workers/WorkerLogout.test.js
+++ frontend/src/Components/Workers/WorkerLogout.test.js
@@ -1,30 +1,29 @@
-  
 import React from 'react';
-import { mount, configure} from 'enzyme';
+import { mount,configure } from 'enzyme';
 import { MemoryRouter } from 'react-router';
 import NavigationBarWorkerPage from '../Layout/NavigationBarWorkerPage';
-import App from '../../App'
+import App from '../../App';
 import Adapter from 'enzyme-adapter-react-16';
-import ShallowRenderer from 'react-test-renderer/shallow';
 import {cleanup} from '@testing-library/react';
+import ShallowRenderer from 'react-test-renderer/shallow';
 
 afterEach(cleanup);
+
 configure({ adapter: new Adapter() });
 
 test('valid path for worker page', () => {
     const wrapper = mount(
-      <MemoryRouter initialEntries={[ '/worker/login' ]}>
+      <MemoryRouter initialEntries={[ '/Worker/login=' ]}>
         <App/>
       </MemoryRouter>
     );
     expect(wrapper.find(<NavigationBarWorkerPage/>)).toHaveLength(0);
   });
 
-it("Worker nav bar is rendered correctly", () =>{
-  const renderer = new ShallowRenderer();
-  renderer.render(<NavigationBarWorkerPage/>);
-  const result = renderer.getRenderOutput();
-  console.log(result);
-  expect(result.type).toBe('div');
-  expect(result.props.children).toBe('ul');
+  it("render admin nav bar correctly", () =>{
+    const renderer = new ShallowRenderer();
+    renderer.render(<NavigationBarWorkerPage/>);
+    const result = renderer.getRenderOutput();
+    expect(result.type).toBe('div');
+    expect(result.props.children.type).toBe('ul');
 });
