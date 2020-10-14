7aa314f19cf06e701caf8c86edb99229f849b9c2
S3718362 <65991825+S3718362@users.noreply.github.com>
2020-10-14 19:07:22 +1100

    Admin backend testing complete

--- BackEnd/src/main/java/com/rmit/sept/mon17305/majorproject/repository/AdminRepository.java
+++ BackEnd/src/main/java/com/rmit/sept/mon17305/majorproject/repository/AdminRepository.java
@@ -1,6 +1,7 @@
 package com.rmit.sept.mon17305.majorproject.repository;
 
 import com.rmit.sept.mon17305.majorproject.model.Admin;
+import com.rmit.sept.mon17305.majorproject.model.Customer;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -16,6 +17,7 @@ public interface AdminRepository extends CrudRepository<Admin, Long> {
     Optional<Admin> findById(Long id);
     Admin findByUsername(String username);
     Admin findByUsernameAndPassword(String username, String password);
+    Admin findByIdEquals(Long id);
     @Override
     void deleteById(Long id);
 }
